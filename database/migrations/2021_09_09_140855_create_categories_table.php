3d9f523d8ea6855eb9cd4de70aea57f32ff7f60f
SusmoySenGupta <susmoy.cse@gmail.com>
2021-09-20 01:13:07 +0600

    feat: add basic models

--- database/migrations/2021_09_09_140855_create_categories_table.php
+++ database/migrations/2021_09_09_140855_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Kalnoy\Nestedset\NestedSet;
 
 class CreateCategoriesTable extends Migration
 {
@@ -18,7 +19,14 @@ class CreateCategoriesTable extends Migration
             $table->unsignedMediumInteger('id', true);
             $table->string('name', 255);
             $table->string('slug', 255)->unique();
-            
+            // $table->unsignedMediumInteger('parent_id')->nullable();
+            $table->nestedSet();
+
+            // $table->foreign('parent_id')
+            // ->references('id')
+            // ->on('categories')
+            // ->onDelete('cascade');
+
             $table->foreignId('created_by')
                 ->nullable()
                 ->constrained('users')
@@ -31,6 +39,7 @@ class CreateCategoriesTable extends Migration
 
             $table->timestamps();
             $table->softDeletes();
+
         });
     }
 
