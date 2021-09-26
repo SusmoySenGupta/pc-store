67f90f9af3360c6be91478078a5c1f11d36595cc
SusmoySenGupta <susmoy.cse@gmail.com>
2021-09-27 01:15:59 +0600

    fix: header issue

--- resources/views/layouts/admin/includes/header.blade.php
+++ resources/views/layouts/admin/includes/header.blade.php
@@ -129,9 +129,9 @@
                         </a>
                     </li>
                     <li class="flex">
-                        <form method="POST" action="{{ route('logout') }}" class="w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200">
+                        <form method="POST" action="{{ route('logout') }}" class="w-full px-2 py-1 text-sm transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200">
                             @csrf
-                            <button type="submit" class="inline-flex items-center w-full"
+                            <button type="submit" class="inline-flex items-center w-full font-semibold"
                                 href="#">
                                 <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none" stroke-linecap="round"
                                     stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
