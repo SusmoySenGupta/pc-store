d5d0ae545bcae50b89501a81529ff9167980be2c
SusmoySenGupta <susmoy.cse@gmail.com>
2021-10-19 00:44:28 +0600

    fix: try catch error not showing

--- resources/views/admin/brand/form.blade.php
+++ resources/views/admin/brand/form.blade.php
@@ -1,4 +1,9 @@
 @csrf
+@if (session()->has('error'))
+    <div class="p-2 bg-red-100 rounded dark:bg-red-300 dark:text-gray-700">
+        {{ session('error') }}
+    </div>
+@endif
 @include('components.forms.inputs.input-text', [
 'attribute' => 'name',
 'is_required' => '',
