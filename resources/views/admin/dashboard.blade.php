7275c103a022ad9737785697b20626d98a73db7a
SusmoySenGupta <susmoy.cse@gmail.com>
2021-10-20 20:53:14 +0600

    feat: add email to dashboard users table

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -79,6 +79,7 @@
                 <thead>
                     <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
                         <th class="px-4 py-3">Users</th>
+                        <th class="px-4 py-3">Email</th>
                         <th class="px-4 py-3">Registered at</th>
                         <th class="px-4 py-3">Status</th>
                         <th class="px-4 py-3">Verified at</th>
@@ -106,6 +107,9 @@
                                         </p>
                                     </div>
                                 </div>
+                            </td>
+                            <td class="px-4 py-3 text-sm">
+                                {{ $user->email }}
                             </td>
                             <td class="px-4 py-3 text-sm">
                                 {{ $user->created_at->format('M d, Y h:i A') }}
