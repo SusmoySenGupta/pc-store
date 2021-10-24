1038b658141281a3df985c84f9e8f92e0adae2f4
SusmoySenGupta <susmoy.cse@gmail.com>
2021-10-24 15:56:38 +0600

    cleanup

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,11 +10,6 @@ use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
-    public function __construct()
-    {
-        // $this->authorizeResource(Category::class, 'category');
-    }
-
     /**
      * Display a listing of the resource.
      *
